15749a0c484529c3f60603aae7a37ad80f1c2c92
Mustafa Ghasemi <mostafa_ghasemi63@yahoo.com>
2019-04-26 14:00:39 -0700

    second and third function pass

--- WordCounter.Tests/ModelTests/Counter.Tests.cs
+++ WordCounter.Tests/ModelTests/Counter.Tests.cs
@@ -9,12 +9,31 @@ namespace WordCountTests
     [TestMethod]
     public void RepeatCounterConstructor_CreateObjectInstance_RepeatCounter()
     {
-        RepeatCounter newRepeatCounter = new RepeatCounter("test");
-        Assert.AreEqual(typeof(RepeatCounter), newRepeatCounter.GetType());
+      RepeatCounter newRepeatCounter = new RepeatCounter();
+      Assert.AreEqual(typeof(RepeatCounter), newRepeatCounter.GetType());
 
     }
+
     [TestMethod]
-    public void SpliteTheSentence_DoesInputedSentenceSplit_False();
-    RepeatCounter newRepeatCounter = new RepeatCounter();
+    public void SpliteTheSentence_DoesInputedSentenceSplit_True()
+    {
+      RepeatCounter newRepeatCounter = new RepeatCounter();
+      string[] expected = { "I", "am", "struggling" };
+      string[] sentenceInput = newRepeatCounter.SpliteTheSentence("I am struggling");
+      Assert.AreEqual(expected[0],sentenceInput[0]);
+      Assert.AreEqual(expected[1],sentenceInput[1]);
+      Assert.AreEqual(expected[2],sentenceInput[2]);
+    }
+
+    [TestMethod]
+    public void SpliteTheSentence_DoesInputedSentenceSplit_False()
+    {
+      RepeatCounter newRepeatCounter = new RepeatCounter();
+      string[] expected = { "we", "am", "struggling" };
+      string[] sentenceInput = newRepeatCounter.SpliteTheSentence("I am struggling");
+      Assert.AreEqual(expected[1],sentenceInput[1]);
+
+    }
+
   }
 }
